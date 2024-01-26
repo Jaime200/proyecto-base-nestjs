ad266eeb1115b30215b85d59eaf59bed1cf2aaab
Jaime Muñoz <jaime.munoz@agexport.org.gt>
2024-01-26 12:04:35 -0600

    Uso de config en servicio

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config'
 @Injectable()
 export class AppService {
   v_tasks: any[];
@@ -8,13 +8,14 @@ export class AppService {
 
   constructor(
     @Inject('TASKS')  v_tasks: any[],
-    @Inject('API_KEY')  apikey: string
+    @Inject('API_KEY')  apikey: string,
+    private configService: ConfigService
   ){
     this.v_tasks = v_tasks;
     this.apikey = apikey
   }
   getHello(): string {
-    return 'Hello World! ' + this.apikey;
+    return `Hello World! ${this.apikey} desde config ${this.configService.get('DATABASE_NAME')}`;
   }
 
   getTasks():any[] {
