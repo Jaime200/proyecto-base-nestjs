70c36f2d91631d88389a956b7bbcd1ce1e7991b6
Jaime Muñoz <jaime.munoz@agexport.org.gt>
2024-01-26 12:27:33 -0600

    Tipado desde archivo de configuracion

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { ColaboradorModule } from './colaborador/colaborador.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config'
 import { enviromets } from '../enviroments'
+import config from '../config'
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath : enviromets[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
     }),
     ColaboradorModule, 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config'
+import { ConfigService, ConfigType } from '@nestjs/config'
+import config from '../config'
 @Injectable()
 export class AppService {
   v_tasks: any[];
@@ -9,13 +10,14 @@ export class AppService {
   constructor(
     @Inject('TASKS')  v_tasks: any[],
     @Inject('API_KEY')  apikey: string,
-    private configService: ConfigService
+    
+    @Inject(config.KEY) private configService : ConfigType<typeof config>
   ){
     this.v_tasks = v_tasks;
     this.apikey = apikey
   }
   getHello(): string {
-    return `Hello World! ${this.apikey} desde config ${this.configService.get('DATABASE_NAME')}`;
+    return `Hello World! ${this.apikey} desde config ${this.configService.database.name}`;
   }
 
   getTasks():any[] {
