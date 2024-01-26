a4a6ce3673b048b530a383a86397819766c9b6b6
Jaime Muñoz <jaime.munoz@agexport.org.gt>
2024-01-26 11:36:44 -0600

    modulo global, [database]

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ColaboradorModule } from './colaborador/colaborador.module';
-import { HttpModule, HttpService } from '@nestjs/axios'
-import { lastValueFrom } from 'rxjs'
-const API_KEY = '123456'
-const API_KEY_PROD = '123456_PROD'
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-  imports: [HttpModule, ColaboradorModule],
+  imports: [ColaboradorModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService,
-  {
-    provide: 'API_KEY',
-    useValue : process.env.NODE_ENV == 'prod'? API_KEY_PROD : API_KEY
-  },
-  {
-    provide : 'TASKS',
-    useFactory : async (http: HttpService) => {
-      const request =  http.get('https://jsonplaceholder.typicode.com/todos');
-      const tasks = await lastValueFrom(request);
-      return tasks.data
-    },
-    inject: [HttpService]
-  },
-],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Global, Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios'
+import { lastValueFrom } from 'rxjs'
+
+const API_KEY = '123456'
+const API_KEY_PROD = '123456_PROD'
+
+@Global()
+@Module({
+    imports: [HttpModule],
+    providers: [
+        {
+            provide: 'API_KEY',
+            useValue : process.env.NODE_ENV == 'prod'? API_KEY_PROD : API_KEY
+          },
+          {
+            provide : 'TASKS',
+            useFactory : async (http: HttpService) => {
+              const request =  http.get('https://jsonplaceholder.typicode.com/todos');
+              const tasks = await lastValueFrom(request);
+              return tasks.data
+            },
+            inject: [HttpService]
+          },
+    ],
+    exports : ['API_KEY', 'TASKS']
+})
+export class DatabaseModule {}
