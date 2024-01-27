132a1b735fa8c13d8257a741c9e8ed4935d75dd5
Jaime Muñoz <jaime.munoz200@gmail.com>
2024-01-27 13:21:03 -0600

    parametrizacion en variables de entorno para bd

--- config.ts
+++ config.ts
@@ -5,6 +5,8 @@ return {
   database : {
         name: process.env.DATABASE_NAME,
         port: process.env.DATABASE_PORT,
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
     },
     apiKey : process.env.API_KEY
     }
--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,8 @@ import * as Joi from 'joi';
       validationSchema : Joi.object({
         DATABASE_NAME : Joi.string().required(),
         DATABASE_PORT : Joi.number().required(),
+        DATABASE_HOST : Joi.string().required(),
+        DATABASE_USER : Joi.string().required(),
         API_KEY : Joi.string().required()
       })
     }),
